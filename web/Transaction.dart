43589952cddf3ce5e0a49fc419ed18afcae8e792
kingroxas2 <59804844+kingroxas2@users.noreply.github.com>
2023-05-01 17:35:50 +0800

    aw
    wd

--- web/Transaction.dart
+++
@@ -1,8 +0,0 @@
-class Transaction {
-  double balance;
-  double payment;
-  DateTime date;
-  double remainingAmount;
-
-  Transaction(this.balance, this.payment, this.date, this.remainingAmount);
-}
